7d61ce516d657c50d8881e65507eee6318f05c50
getchards <kazansasha02@gmail.com>
2024-06-14 12:55:28 +0300

    make build

---
+++ index.db1bfbfe.js
@@ -0,0 +1,2 @@
+var t=document.querySelector(".spider"),e=document.querySelector(".wall"),l=t.clientWidth,c=t.clientHeight,i=e.clientWidth,o=e.clientHeight;t.style.position="absolute",t.style.top="".concat((o-c)/2,"px"),t.style.left="".concat((i-l)/2,"px");
+//# sourceMappingURL=index.db1bfbfe.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,14 @@
+"use strict";
+const spider = document.querySelector(".spider");
+const wall = document.querySelector(".wall");
+const spiderWidth = spider.clientWidth;
+const spiderHeight = spider.clientHeight;
+const wallWidth = wall.clientWidth;
+const wallHeight = wall.clientHeight;
+const topPosition = (wallHeight - spiderHeight) / 2;
+const leftPosition = (wallWidth - spiderWidth) / 2;
+spider.style.position = "absolute";
+spider.style.top = `${topPosition}px`;
+spider.style.left = `${leftPosition}px`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
